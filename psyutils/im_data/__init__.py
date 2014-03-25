99cb3fd0503abc42be767cb5e09831b67c8639b8
tomwallis <tsawallis@gmail.com>
2014-03-25 15:49:38 +0100

    added some images for testing functions.

---
+++ psyutils/im_data/__init__.py
@@ -0,0 +1,60 @@
+"""Test images.
+
+Much of the code in here was taken from skimage.data.__init__.py
+
+Tiger
+---------
+Copyright flickr user Phalinn Ooi, released under a CC by 2.0 license. 
+This image can be freely shared and adapted.
+License: https://creativecommons.org/licenses/by/2.0/legalcode
+
+
+"""
+
+import os as _os
+
+from skimage.io import imread as _imread
+from psyutils import _im_data_dir
+
+
+
+__all__ = ['tiger',
+           'tiger_square'
+           ]
+
+
+def _load(f):
+    """Load an image file located in the data directory.
+
+        
+    Parameters
+    ----------
+    f : string
+        File name.
+
+    Returns
+    -------
+    img : ndarray
+    Image loaded from psyutils.im_data_dir.
+    """
+    return _imread(_os.path.join(_im_data_dir, f))
+
+
+def tiger():
+    """Tiger image by Phalinn Ooi (flickr).
+    Released and used here under a CC by 2.0 license.
+    License: https://creativecommons.org/licenses/by/2.0/legalcode
+    """
+    return _load("tiger.png")
+
+def tiger_rgba():
+    """Tiger image with alpha channel.
+    """
+    return _load("tiger.png")    
+
+def tiger_square():
+    """Square version of tiger image (RGB).
+    """
+    return _load("tiger_square.png")
+
+
