85414925173243dde2fc3e926873098f079158e9
tomwallis <tsawallis@gmail.com>
2014-03-26 17:37:54 +0100

    first unit tests (of test images)

---
+++ psyutils/tests/im_data_tests.py
@@ -0,0 +1,51 @@
+# tests for image data.
+
+import numpy as np
+from skimage import img_as_float
+import psyutils as pu
+
+def test_tiger_square():
+    im = img_as_float(pu.im_data.tiger_square())
+    correct_type = "<class 'numpy.ndarray'>"
+    string_type = str(type(im))
+    min_range = round(im.min(), ndigits=2)
+    max_range = round(im.max(), ndigits=2)
+    mean = round(im.mean(), ndigits=2)
+    sd = round(im.std(), ndigits=2)
+    assert string_type == correct_type \
+        and im.shape == (256, 256, 3) \
+        and min_range == 0.01 \
+        and max_range == 1.0 \
+        and mean == 0.45 \
+        and sd == 0.23
+
+def test_tiger_grey():
+    im = img_as_float(pu.im_data.tiger_grey())
+    correct_type = "<class 'numpy.ndarray'>"
+    string_type = str(type(im))
+    min_range = round(im.min(), ndigits=2)
+    max_range = round(im.max(), ndigits=2)
+    mean = round(im.mean(), ndigits=2)
+    sd = round(im.std(), ndigits=2)
+    assert string_type == correct_type \
+        and im.shape == (256, 256, 3) \
+        and min_range == 0.0 \
+        and max_range == 1.0 \
+        and mean == 0.51 \
+        and sd == 0.2
+
+def test_tiger_rgba():
+    im = img_as_float(pu.im_data.tiger_rgba())
+    correct_type = "<class 'numpy.ndarray'>"
+    string_type = str(type(im))
+    min_range = round(im.min(), ndigits=2)
+    max_range = round(im.max(), ndigits=2)
+    mean = round(im.mean(), ndigits=2)
+    sd = round(im.std(), ndigits=2)
+    assert string_type == correct_type \
+        and im.shape == (427, 640, 4) \
+        and min_range == 0.0 \
+        and max_range == 1.0 \
+        and mean == 0.54 \
+        and sd == 0.32
+
