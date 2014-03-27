78d4b03ff8b71bc1d3e0035268e92c65decd5638
tomwallis <tsawallis@gmail.com>
2014-03-27 15:13:48 +0100

    wrote tests for contrast image function.

--- tests/image_utils_tests.py
+++ tests/image_utils_tests.py
@@ -1,7 +1,7 @@
 # run unit tests on image utilities.
 
 import numpy as np
-#from skimage import img_as_float
+from skimage import img_as_float
 import psyutils as pu
 from nose.tools import *
 
@@ -39,6 +39,53 @@ def test_guess_type_6():
 
 ############# Testing contrast_image ##############
 
+def test_contrast_image_1():
+    im = img_as_float(pu.im_data.tiger())
+    im = pu.image.contrast_image(im, factor=0.2)
+    min_range = round(im.min(), ndigits=2)
+    max_range = round(im.max(), ndigits=2)
+    mean = round(im.mean(), ndigits=2)
+    sd = round(im.std(), ndigits=2)
+    assert min_range == 0.21 \
+        and max_range == 0.59 \
+        and mean == 0.39 \
+        and sd == 0.1
+
+def test_contrast_image_2():
+    im = img_as_float(pu.im_data.tiger())
+    im = pu.image.contrast_image(im, factor=0.2, returns = "contrast")
+    min_range = round(im.min(), ndigits=2)
+    max_range = round(im.max(), ndigits=2)
+    mean = round(im.mean(), ndigits=2)
+    sd = round(im.std(), ndigits=2)
+    assert min_range == -0.1 \
+        and max_range == 0.15 \
+        and mean == -0.0 \
+        and sd == 0.04
+
+def test_contrast_image_3():
+    im = img_as_float(pu.im_data.tiger_grey())
+    im = pu.image.contrast_image(im, factor=0.1)
+    min_range = round(im.min(), ndigits=2)
+    max_range = round(im.max(), ndigits=2)
+    mean = round(im.mean(), ndigits=2)
+    sd = round(im.std(), ndigits=2)
+    assert min_range == 0.46 \
+        and max_range == 0.56 \
+        and mean == 0.51 \
+        and sd == 0.02
+
+def test_contrast_image_4():
+    im = img_as_float(pu.im_data.tiger_grey())
+    im = pu.image.contrast_image(im, factor=0.1, returns = "contrast")
+    min_range = round(im.min(), ndigits=2)
+    max_range = round(im.max(), ndigits=2)
+    mean = round(im.mean(), ndigits=2)
+    sd = round(im.std(), ndigits=2)
+    assert min_range == -0.05 \
+        and max_range == 0.05 \
+        and mean == 0.0 \
+        and sd == 0.02
 
 # See
 # http://docs.scipy.org/doc/numpy-dev/reference/generated/numpy.testing.assert_allclose.html#numpy.testing.assert_allclose
