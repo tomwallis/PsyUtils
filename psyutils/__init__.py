55317f088bcf87acf77f7f2663a2371546aacb47
tomwallis <tsawallis@gmail.com>
2014-03-27 13:12:22 +0100

    do these directories within the submodules, since they don't work with sphinx.

--- psyutils/__init__.py
+++ psyutils/__init__.py
@@ -1,8 +1,4 @@
 import os.path as _osp
-
-_pkg_dir = _osp.abspath(_osp.dirname(__file__))
-_im_data_dir = _osp.join(_pkg_dir, 'im_data')
-
 import psyutils.data
 import psyutils.image
 import psyutils.misc
