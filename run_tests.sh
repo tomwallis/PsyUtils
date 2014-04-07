05a24abbdcfaa83dbed114e23099522c871c8f1e
tomwallis <tsawallis@gmail.com>
2014-04-07 11:32:28 +0200

    shell script to run nosetests with options.

---
+++ run_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# run tests for psyutils, in both python 2 and 3 (assuming you're in the package root directory).
+nosetests -v --nocapture
+read -p "That was Python 2. Press [Enter] key to run tests under Python 3..."
+nosetests-3.3 --py3where=./ -v --nocapture
