094ce72fa830bc7d101d3ce63ab24675c9b84169
tomwallis <tsawallis@gmail.com>
2014-04-10 13:08:08 +0200

    made some changes required to install with pip

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ setup(
     version='0.1.0',
     author='Thomas S. A. Wallis',
     author_email='thomas.wallis@uni-tuebingen.de',
-    packages=['psyutils', 'psyutils.data', 'psyutils.image', 'psyutils.misc', 'psyutils.im_data'],
+    packages=['psyutils', 'psyutils.data', 'psyutils.image', 'psyutils.misc',
+              'psyutils.im_data'],
     #scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
     #url='http://pypi.python.org/pypi/TowelStuff/',
     license='LICENSE.txt',
-    description='Utility functions for psychophysical experiments and stimuli.',
-    long_description=open('README.txt').read(),
+    description='Utility functions for psychophysical experiments and \
+                stimuli.',
+    long_description=open('README.md').read(),
     install_requires=[
-        'Python >= 2.7.0',
+        # 'Python >= 2.7.0',
         'numpy >= 1.8.0',
         'scipy >= 0.13.0',
         'matplotlib',
-        'skimage >= 0.9.0'
+        'scikit-image >= 0.9.0'
     ],
 )
