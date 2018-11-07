c891f72101a8a564ead669d41de2b95432c762fa
Tom Wallis <tomwallis@users.noreply.github.com>
2018-11-07 14:26:34 +0100

    rollback initi.

--- psyutils/__init__.py
+++ psyutils/__init__.py
@@ -1,6 +1,6 @@
-# from . import image
-# from . import misc
-# from . import im_data
-# from . import dist
-# from . import files
-# from . import psydata
+from . import image
+from . import misc
+from . import im_data
+from . import dist
+from . import files
+from . import psydata
